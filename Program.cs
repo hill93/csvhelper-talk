e4be0357caaa8c3cc5ec3e38f843dc264072f5f7
Henry Hill <henryrichardhill@gmail.com>
2024-07-21 11:39:09 +0100

    Added .gitignore, moved food stuff to demo file

--- Program.cs
+++ Program.cs
@@ -1,41 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using CvsHelperTalk.Csv.Entities;
-using CvsHelperTalk.Csv.Readers;
-using CvsHelperTalk.Csv.Writers;
+using CvsHelperTalk.Demos;
 
-var foodItemReader = new FoodItemReader();
-var foodItemWriter = new FoodItemWriter();
+var foodDemo = new FoodDemo();
 
-var items = foodItemReader.Read();
-
-//Add a new item
-var item = new FoodItem
-{
-    Id = 4,
-    Name = "Peanut Butter",
-    Description = "One of them big tubs, very nice",
-    Price = 6.5M,
-    Unit = CvsHelperTalk.Csv.Enum.Unit.Grams,
-    Quantity = 1000,
-    DateAdded = DateTime.Now,
-    InStock = true,
-    NutritionalInfo = new()
-    {
-        Energy = 2673,
-        Protein = 26.3M,
-        Carbohydrates = 9.2M,
-        Sugars = 4.2M
-    }
-};
-
-items.Add(item);
-
-foodItemWriter.AppendRange(new() { item});
-
-//Update items - milk now in stock!
-var milk = items.FirstOrDefault(x => x.Name == "Milk");
-
-milk.InStock = true;
-
-foodItemWriter.WriteRange(items);
+foodDemo.Run();
